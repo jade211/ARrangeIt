af091ae6dacdb2cf5c5ed1a3150098e6376eaa8f
hudsonj5 <jade.hudson5@mail.dcu.ie>
2025-01-23 21:53:43 +0000

    Add arcore functionality added back into the code after the user logs in

--- src/ARrangeIt/app/src/main/java/com/example/arrangeit/HomePage.java
+++ src/ARrangeIt/app/src/main/java/com/example/arrangeit/HomePage.java
@@ -1,24 +1,29 @@
 package com.example.arrangeit;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class HomePage extends AppCompatActivity {
 
+    Button ar_core;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ar_core = findViewById(R.id.ar_core);
+        ar_core.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, ARCorePage.class);
+                startActivity(intent);
+                finish();
+            }
         });
+
     }
 }
