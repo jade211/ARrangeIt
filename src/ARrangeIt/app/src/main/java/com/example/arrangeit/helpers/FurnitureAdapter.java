7270306cac8d46b4f5f7180800b8c6630f55ff23
Sruthi Santhosh <sruthi.santhosh2@mail.dcu.ie>
2025-04-30 21:54:35 +0100

    Removed unused libraries Furniture Adapter

--- src/ARrangeIt/app/src/main/java/com/example/arrangeit/helpers/FurnitureAdapter.java
+++ src/ARrangeIt/app/src/main/java/com/example/arrangeit/helpers/FurnitureAdapter.java
@@ -1,7 +1,6 @@
 package com.example.arrangeit.helpers;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
